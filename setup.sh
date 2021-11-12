28255e62bd4f0719f1c233f2296c2a5ee54ee652
Volkan Gül <ugurvolkangul@gmail.com>
2021-11-11 19:42:39 -0800

    Update install script

--- setup.sh
+++ setup.sh
@@ -24,7 +24,7 @@ then
     fi
     chsh -s $(which zsh)
 fi
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 ./setup-symlinks.sh
