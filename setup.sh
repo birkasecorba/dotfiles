fe607febecd64a60e302fc1183a9b778a9778704
Volkan Gül <ugurvolkangul@gmail.com>
2021-11-11 20:42:07 -0800

    Update ohmyzsh install

--- setup.sh
+++ setup.sh
@@ -24,7 +24,7 @@ then
     fi
     chsh -s $(which zsh)
 fi
-curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)
 # git clone https://github.com/zsh-users/zsh-autosuggestions ${HOME}/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 ~/.dotfiles/setup-symlinks.sh
